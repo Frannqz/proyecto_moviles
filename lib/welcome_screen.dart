b462734c331268fd0348185a1993e35270b757c2
CristianCM17 <cristiandavidcardoso@gmail.com>
2024-03-12 01:32:02 -0600

    Add slogan

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -5,41 +5,55 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 28, right: 36, top: 160),
+            padding: const EdgeInsets.only(left: 30, right: 36, top: 160),
             child: SizedBox(
               width: 311,
-              height: 130,
-              child: Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Bienvenido a\n',
-                      style: TextStyle(
-                        color: Color(0xFF111719),
-                        fontSize: 45,
-                        fontFamily: 'Sofia Pro',
-                        fontWeight: FontWeight.w700,   
+              height: 184,
+              child: Column(children: [
+                const Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Bienvenido a\n',
+                        style: TextStyle(
+                          color: Color(0xFF111719),
+                          fontSize: 45,
+                          fontFamily: 'Sofia Pro',
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
-                    TextSpan(
-                      text: 'La Pasadita',
-                      style: TextStyle(
-                        color: Color(0xFFFE724C),
-                        fontSize: 45,
-                        fontFamily: 'Sofia Pro',
-                        fontWeight: FontWeight.w700,
-                       
+                      TextSpan(
+                        text: 'La Pasadita',
+                        style: TextStyle(
+                          color: Color(0xFFFE724C),
+                          fontSize: 45,
+                          fontFamily: 'Sofia Pro',
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
+                Container(
+                  padding: EdgeInsets.only(right: 43),
+                  width: 266,
+                  child: const Text(
+                    'Somos la Diferencia en Comida Rápida en Celaya',
+                    style: TextStyle(
+                      color: Color(0xFF2F384E),
+                      fontSize: 18,
+                      fontFamily: 'Sofia Pro',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                )
+              ]),
             ),
-          )
+          ),
         ],
       ),
     );
